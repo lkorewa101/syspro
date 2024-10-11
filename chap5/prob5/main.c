fb913ab6eb35e1d0ef0bb4e7044d4f7c575409d7
lkorewa101 <lkorewa101@gmail.com>
2024-10-11 09:33:07 +0000

    prob all complete

--- chap5/prob5/main.c
+++ chap5/prob5/main.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
 		exit(1);
 	}
 
-	if((fd == open(argv[1], O_WRONLY | O_CREAT | O_EXCL, 0640)) == -1) {
+	if((fd == open(argv[1], O_WRONLY|O_CREAT|O_EXCL, 0640)) == -1) {
 		perror(argv[1]);
 		exit(2);
 	}
--- chap5/prob6/main.c
+++ chap5/prob6/main.c
@@ -17,4 +17,16 @@ int main(int argc, char *argv[]) {
 		exit(2);
 	}
 	do {
-
+		printf("\Enter StudentID to serach:");
+		if (scanf("%d", &id) == 1) {
+			lseek(fd, (id-START_ID)*sizeof(record), SEEK_SET);
+			if((read(fd, (char *) &record, sizeof(record)) > 0) && (record.id != 0))
+				printf("Name:%s\t StuID:%d\t Score:%d\n", record.name, record.id, record.score);
+			else printf("Record %d Null\n", id);
+		} else printf("Input Error");
+		printf("Continue?(Y/N)");
+		scanf(" %c", &c);
+	} while(c == 'Y');
+	close(fd);
+	exit(0);
+}
