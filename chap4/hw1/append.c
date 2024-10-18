982dfb6e32ca7b4621eb499cbc6bda0202348cae
lkorewa101 <lkorewa101@gmail.com>
2024-10-18 11:43:16 +0000

    chap4 hw2 complete

--- chap4/hw1/append.c
+++ chap4/hw1/append.c
@@ -1,41 +1,46 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     char c;
     FILE *fp1, *fp2;
 
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s file1 file2\n", argv[0]);
+        fprintf(stderr, "How to use: %s File1 File2\n", argv[0]);
         return 1;
     }
 
     fp1 = fopen(argv[1], "r");
     if (fp1 == NULL) {
-        fprintf(stderr, "File %s open error\n", argv[1]);
+        fprintf(stderr, "File %s Open Error\n", argv[1]);
         return 2;
     }
 
-    fp2 = fopen(argv[2], "a+");
+    fp2 = fopen(argv[2], "a");
     if (fp2 == NULL) {
-        fprintf(stderr, "File %s open error\n", argv[2]);
+        fprintf(stderr, "File %s Open Error\n", argv[2]);
         fclose(fp1);
         return 3;
     }
 
-    fseek(fp2, -1, SEEK_END);
-    if (fgetc(fp2) == '\n') {
-        fseek(fp2, 0, SEEK_END); 
-    } else {
-        fseek(fp2, 0, SEEK_END); 
+    fseek(fp2, 0, SEEK_END);
+    long pos = ftell(fp2);
+    if (pos > 0) {
+        fseek(fp2, -1, SEEK_END);
+        if (fgetc(fp2) != '\n') {
+            fseek(fp2, 0, SEEK_END);
+            fputc('\n', fp2);
+        }
     }
 
+    fseek(fp2, 0, SEEK_END);
+    
     while ((c = fgetc(fp1)) != EOF) {
         fputc(c, fp2);
     }
 
     fclose(fp1);
     fclose(fp2);
-
     return 0;
 }
 
--- chap4/hw2/cat.c
+++ chap4/hw2/cat.c
@@ -1,17 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLINE 80
 
 int main(int argc, char *argv[]) {
-	FILE *fp;
-	int c;
-	if(argc < 2)
-		fp = stdin;
-	else fp = fopen(argv[1],"r");
+    FILE *fp;
+    int c;
+    int line_number = 0;
+    int print_line_numbers = 0;
 
-	c = getc(fp);
-	while(c != EOF) {
-		putc(c, stdout);
-		c = getc(fp);
-	}
-	fclose(fp);
-	return 0;
+    if (argc > 1 && strcmp(argv[1], "-n") == 0) {
+        print_line_numbers = 1;
+        argv++;
+        argc--;
+    }
+
+    if (argc < 2) {
+        fp = stdin;
+        if (print_line_numbers) {
+            fprintf(stderr, "No files specified. Reading from standard input.\n");
+        }
+        c = getc(fp);
+        while (c != EOF) {
+            putc(c, stdout);
+            c = getc(fp);
+        }
+        fclose(fp);
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if ((fp = fopen(argv[i], "r")) == NULL) {
+            fprintf(stderr, "Error opening file: %s\n", argv[i]);
+            continue;
+        }
+
+        if (print_line_numbers) {
+            char buffer[MAXLINE];
+            int line = 0;
+
+            while (fgets(buffer, MAXLINE, fp) != NULL) {
+                line++;
+                printf("%d %s", line, buffer);
+            }
+        } else {
+            c = getc(fp);
+            while (c != EOF) {
+                putc(c, stdout);
+                c = getc(fp);
+            }
+        }
+
+        fclose(fp);
+    }
+
+    return 0;
 }
+
