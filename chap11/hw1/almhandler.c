c83aaeb8c326fcb6c9ea7e4ce3403468001fca4d
lkorewa101 <lkorewa101@gmail.com>
2024-11-28 04:07:23 +0000

    chap11 complete

--- chap11/hw1/almhandler.c
+++ chap11/hw1/almhandler.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
-
-void  alarmHandler();
-
-int main() {
-	signal(SIGALRM, alarmHandler);
-	alarm(5);
-	short i = 0;
-	while (1) {
-		sleep(1);
-		i++;
-		printf("%d second\n", i);
-	}
-	printf("end\n");
-}
+#include <unistd.h>
 
 void alarmHandler(int signo) {
-	printf("Wake up\n");
-	exit(0);
+    printf("Wake up\n");
+    exit(0);
+}
+
+int mySignal(int signo, void (*handler)(int)) {
+    struct sigaction sa;
+
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    return sigaction(signo, &sa, NULL);
+}
+
+int main() {
+    mySignal(SIGALRM, alarmHandler);
+
+    alarm(5);
+
+    short i = 0;
+    while (1) {
+        sleep(1);
+        i++;
+        printf("%d second\n", i);
+    }
+
+    printf("end\n");
+    return 0;
 }
